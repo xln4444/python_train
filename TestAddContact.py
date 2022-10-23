9fad58299f52b4c6120c8b48ecaa83b652f846ef
Павел <parfenov.pv@idp.zyfra.com>
2022-10-23 14:08:01 +0300

    Выделены вспомогательные методы

--- TestAddContact.py
+++ TestAddContact.py
@@ -1,11 +1,9 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+import unittest
 
 class TestAddContact(unittest.TestCase):
     def setUp(self):
@@ -14,54 +12,45 @@ class TestAddContact(unittest.TestCase):
 
     def test_add_contact(self):
         wdr = self.wdr
+        self.open_home_page(wdr)
+        self.login(wdr)
+        self.open_add_contact_page(wdr)
+        self.create_contact(wdr)
+        self.goto_home_page(wdr)
+
+    def open_home_page(self, wdr):
         # открытие домашней страницы
         wdr.get("http://localhost/addressbook/edit.php")
+
+    def login(self, wdr):
         # авторизация
         wdr.find_element_by_name("user").click()
         wdr.find_element_by_name("user").clear()
         wdr.find_element_by_name("user").send_keys("admin")
         wdr.find_element_by_name("pass").clear()
         wdr.find_element_by_name("pass").send_keys("secret")
-        wdr.find_element_by_xpath("//input[@value='Login']").click()
+        wdr.find_element_by_id("LoginForm").submit()
+
+    def open_add_contact_page(self, wdr):
         # открытие страницы добавления нового контакта
         wdr.find_element_by_link_text("add new").click()
+
+    def create_contact(self, wdr):
         # заполнение полей формы
-        wdr.find_element_by_name("firstname").click()
-        wdr.find_element_by_name("firstname").clear()
         wdr.find_element_by_name("firstname").send_keys("First")
-        wdr.find_element_by_name("middlename").clear()
         wdr.find_element_by_name("middlename").send_keys("Middle")
-        wdr.find_element_by_name("lastname").clear()
         wdr.find_element_by_name("lastname").send_keys("Last")
-        wdr.find_element_by_name("nickname").clear()
         wdr.find_element_by_name("nickname").send_keys("Nick")
-        wdr.find_element_by_name("photo").click()
-        wdr.find_element_by_name("photo").clear()
-        wdr.find_element_by_name("photo").send_keys("C:\\fakepath\\kotik kotorogo ne vidno.jpg")
-        wdr.find_element_by_name("title").click()
-        wdr.find_element_by_name("title").clear()
         wdr.find_element_by_name("title").send_keys("Zagolovok")
-        wdr.find_element_by_name("company").clear()
         wdr.find_element_by_name("company").send_keys("Company")
-        wdr.find_element_by_name("address").clear()
         wdr.find_element_by_name("address").send_keys("Address")
-        wdr.find_element_by_name("home").clear()
         wdr.find_element_by_name("home").send_keys("home")
-        wdr.find_element_by_name("mobile").clear()
         wdr.find_element_by_name("mobile").send_keys("Mobile")
-        wdr.find_element_by_name("work").clear()
         wdr.find_element_by_name("work").send_keys("Work")
-        wdr.find_element_by_name("fax").click()
-        wdr.find_element_by_name("fax").clear()
-        wdr.find_element_by_name("fax").send_keys("Fax (kto im seychas pol'zuetsia voobsh'e?)")
-        wdr.find_element_by_name("email").click()
-        wdr.find_element_by_name("email").clear()
+        wdr.find_element_by_name("fax").send_keys("Fax")
         wdr.find_element_by_name("email").send_keys("e-mail1")
-        wdr.find_element_by_name("email2").clear()
         wdr.find_element_by_name("email2").send_keys("e-mail2")
-        wdr.find_element_by_name("email3").clear()
         wdr.find_element_by_name("email3").send_keys("e-mail3")
-        wdr.find_element_by_name("homepage").clear()
         wdr.find_element_by_name("homepage").send_keys("Homepage")
         wdr.find_element_by_name("bday").click()
         Select(wdr.find_element_by_name("bday")).select_by_visible_text("17")
@@ -81,32 +70,33 @@ class TestAddContact(unittest.TestCase):
         wdr.find_element_by_name("ayear").click()
         wdr.find_element_by_name("ayear").clear()
         wdr.find_element_by_name("ayear").send_keys("2020")
-        wdr.find_element_by_name("new_group").click()
-        wdr.find_element_by_name("theform").click()
         # заполнение блока дополнительной информации (Secondary)
         wdr.find_element_by_name("address2").click()
         wdr.find_element_by_name("address2").clear()
         wdr.find_element_by_name("address2").send_keys("Moscow")
         wdr.find_element_by_name("phone2").clear()
-        wdr.find_element_by_name("phone2").send_keys("sweet home")
+        wdr.find_element_by_name("phone2").send_keys("+79055405959")
         wdr.find_element_by_name("notes").clear()
         wdr.find_element_by_name("notes").send_keys("-")
         # создание контакта
-        wdr.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
-    
+        wdr.find_element_by_name("submit").click()
+
+    def goto_home_page(self, wdr):
+        # переход на домашнюю страницу
+        wdr.find_element_by_link_text("home page").click()
+
     def is_element_present(self, how, what):
-        try: self.wdd.find_element(by=how, value=what)
+        try: self.wdr.find_element(by=how, value=what)
         except NoSuchElementException as e: return False
         return True
     
     def is_alert_present(self):
-        try: self.wdd.switch_to_alert()
+        try: self.wdr.switch_to_alert()
         except NoAlertPresentException as e: return False
         return True
     
     def tearDown(self):
-        self.wdd.quit()
-        self.assertEqual([], self.verificationErrors)
+        self.wdr.quit()
 
 if __name__ == "__main__":
     unittest.main()

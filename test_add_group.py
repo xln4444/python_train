050c6d32878d58d0de957b80ffa4c2d8fab0f125
Павел <parfenov.pv@idp.zyfra.com>
2022-11-01 23:33:52 +0300

    Создание класса Application, перенос методов взаимодействующих с веб драйвером в класс Application. Изменен класс tes add group - удалены методы с wd, добавлен метод app, оставшиеся отредактированы для работы с app

--- test_add_group.py
+++ test_add_group.py
@@ -1,85 +1,22 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 from python_train.group import Group
-import unittest
+from application import Application
+import pytest
 
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
-class TestAddGroup(unittest.TestCase):
-    def setUp(self):
-        self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
-    
-    def test_add_group(self):
-        wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd, username="admin", password="secret")
-        self.open_groups_page(wd)
-        self.create_group(wd, Group(name="QWE", header="ASD", footer="ZXC"))
-        self.return_to_groups_page(wd)
-        self.logout(wd)
+def test_add_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="QWE", header="ASD", footer="ZXC"))
+    app.logout()
 
-    def test_add_empty_group(self):
-        wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd, username="admin", password="secret")
-        self.open_groups_page(wd)
-        self.create_group(wd, Group(name="", header="", footer=""))
-        self.return_to_groups_page(wd)
-        self.logout(wd)
-
-    def logout(self, wd):
-        # выход
-        wd.find_element_by_link_text("Logout").click()
-
-    def return_to_groups_page(self, wd):
-        # переход на страницу групп
-        wd.find_element_by_link_text("group page").click()
-
-    def create_group(self, wd, group):
-        # создание новой группы
-        wd.find_element_by_name("new").click()
-        # заполнение полей формы
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-        # создание группы
-        wd.find_element_by_name("submit").click()
-
-    def open_groups_page(self, wd):
-        # открытие страницы групп
-        wd.find_element_by_link_text("groups").click()
-
-    def login(self, wd, username, password):
-        # авторизация
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-
-    def open_home_page(self, wd):
-        # открытие домашней страницы
-        wd.get("http://localhost/addressbook")
-
-    def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-    
-    def is_alert_present(self):
-        try: self.wd.switch_to_alert()
-        except NoAlertPresentException as e: return False
-        return True
-    
-    def tearDown(self):
-        self.wd.quit()
-
-if __name__ == "__main__":
-    unittest.main()
+def test_add_empty_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="", header="", footer=""))
+    app.logout()
